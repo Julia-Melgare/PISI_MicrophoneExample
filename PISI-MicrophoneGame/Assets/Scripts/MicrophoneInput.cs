51ae6971630cab744c8777ea7dcc930aeaf84e9f
Julia Melgare <julia.melgare@acad.pucrs.br>
2023-05-01 17:52:45 -0300

    updated version and added microphone bug workaround

--- PISI-MicrophoneGame/Assets/Scripts/MicrophoneInput.cs
+++ PISI-MicrophoneGame/Assets/Scripts/MicrophoneInput.cs
@@ -11,12 +11,8 @@ public class MicrophoneInput : MonoBehaviour
 
     void Start()
     {
-        //GetComponent<AudioSource>().clip = Microphone.Start(null, true, 10, 44100);
         audioSource.clip = Microphone.Start(null, true, 10, 44100);
-        //GetComponent<AudioSource>().loop = true; // Set the AudioClip to loop
-        audioSource.loop = true;
-        //GetComponent<AudioSource>().mute = true; // Mute the sound, we don't want the player to hear it
-        audioSource.mute = true;
+        audioSource.loop = true; // Set the AudioClip to loop
         while (!(Microphone.GetPosition("") > 0))
         {
         } // Wait until the recording has started
@@ -32,13 +28,10 @@ public class MicrophoneInput : MonoBehaviour
     {
         float[] data = new float[256];
         float a = 0;
-        //GetComponent<AudioSource>().GetOutputData(data, 0);
         audioSource.GetOutputData(data, 0);
         foreach (float s in data)
         {
             a += Mathf.Abs(s);
-            //Debug.Log("a " + a);
-            //Debug.Log("/a " + a/256);
         }
         return a / 256;
     }
